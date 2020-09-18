4f8cb6ce09f36f6e658428d61746bbca73b41cf7
FarihaNusrat460 <fariha17nusrat@gmail.com>
2020-09-19 01:11:42 +0600

    Artwork
    artwork class is created

--- Artwork/models.py
+++ Artwork/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Artworks(models.Model):
+    title=models.CharField(max_length=100,default="")
+    status=models.CharField(max_length=100,default="")
+    medium=models.CharField(max_length=100,default="")
+    category=models.CharField(max_length=100,default="")
+
+    def __str__(self):
+
+        return self.title
+
+
+
+
