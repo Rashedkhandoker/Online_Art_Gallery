6bcff95bf2bcfc702a322ed0df0930cec03f172b
PiasTanmoy <pias1600@gmail.com>
2020-09-21 15:23:21 +0600

    ignore

--- Blog/models.py
+++ Blog/models.py
@@ -6,13 +6,10 @@ from Profile.models import User
 class Blog(models.Model):
     title = models.CharField(max_length=100, default="")
     bloggers_name = models.CharField(max_length=100, default="")
-
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
 class Post(models.Model):
-    email = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together=(('email', 'blog_id'),)
