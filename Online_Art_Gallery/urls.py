4288ca1d6bde584b439ac01da5ae7b566e4d8c2f
BrishtyBilash <67582453+BrishtyBilash@users.noreply.github.com>
2020-09-27 00:58:13 +0600

    login/logout

--- Online_Art_Gallery/urls.py
+++ Online_Art_Gallery/urls.py
@@ -14,23 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from Profile import views as ProfileView
 from Artwork  import views as Artworkview
 from Blog import views as Blog_views
 from Event import views as EventView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AddUser/', ProfileView.showUser), #User Show er jonno
-    path('signup/', ProfileView.registration),
-    path('InsertUsers/', ProfileView.insertUser), #User insert er table dekhabe
-    path('ShowArtwork/',Artworkview. showArtwork),
-    path('insertArtwork/',Artworkview.insertArtwork),
-    path('showBlogs/', Blog_views.showBlog),
-    path('InsertBlogs/', Blog_views.insertBlog),
-    path('InsertUsers/', ProfileView.insertUser),#User insert er table dekhabe
-    path('Exhibition/',EventView.showevent),
-    path('Competition/',EventView.showcompetition)
+    path('AddUser/', ProfileView.showUser,name='AddUser'), #User Show er jonno
+    path('signup/', ProfileView.registration,name='signup'),
+    #path('InsertUsers/', ProfileView.insertUser), #User insert er table dekhabe
+    path('ShowArtwork/',Artworkview. showArtwork,name='ShowArtwork'),
+    path('InsertArtwork/',Artworkview.insertArtwork,name='InsertArtwork'),
+    path('ShowBlogs/', Blog_views.showBlog,name='ShowBlogs'),
+    path('InsertBlogs/', Blog_views.insertBlog,name='InsertBlog'),
+    path('InsertUsers/', ProfileView.insertUser,name='InsertUsers'),#User insert er table dekhabe
+    path('Exhibition/',EventView.showevent,name='Exhibition'),
+    path('Competition/',EventView.showcompetition,name='Competition'),
+    path('accounts/',include('django.contrib.auth.urls'))
 
 ]
