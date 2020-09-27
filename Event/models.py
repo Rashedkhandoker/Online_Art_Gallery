cc244d2eaa53db560c580df95caad5ee4d75dc50
Rashed Khandoker <rashedrony02@gmail.com>
2020-09-27 21:55:44 +0600

    datetime

--- Event/models.py
+++ Event/models.py
@@ -4,17 +4,15 @@ from django.db import models
 # Create your models here.
 
 class Exhibition(models.Model):
-    exh_id = models.CharField(max_length=100)
     date = models.DateTimeField()
 
     def __str__(self):
-        return self.exh_id
+        return self.id
 
 
 class Competition(models.Model):
-    com_id = models.CharField(max_length=100)
-    date = models.CharField(max_length=100)
-    time = models.CharField(max_length=100)
+
+    date = models.DateTimeField()
 
     def __str__(self):
-        return self.com_id
+        return self.id
