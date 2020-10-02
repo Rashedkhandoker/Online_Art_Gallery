4775586cabbf469d64d6e66bb902fbec3697dc51
KashfiaJashim <18101046@uap-bd.edu>
2020-10-02 23:44:45 +0600

    Homepage designed

--- Online_Art_Gallery/settings.py
+++ Online_Art_Gallery/settings.py
@@ -17,7 +17,6 @@ from django.template.context_processors import media
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'j$^+0_7pc8-$)%v^x+2o)@5=o=9es!oq535rtm*_j!un6498nx'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -44,7 +42,6 @@ INSTALLED_APPS = [
     'Event.apps.EventConfig',
     'Blog.apps.BlogConfig',
     'crispy_forms'
-
 
 ]
 
@@ -81,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Online_Art_Gallery.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -91,7 +87,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -111,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -125,7 +119,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -135,3 +128,4 @@ LOGOUT_REDIRECT_URL = 'login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
